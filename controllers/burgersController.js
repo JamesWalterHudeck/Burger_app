2764b72e055a449839b26526b7ee1a1dd6839252
James Hudeck <JamesWalterHudeck717@gmail.com>
2020-05-17 13:07:38 -0400

    Burger App v1.0.0

---
+++ controllers/burgersController.js
@@ -0,0 +1,40 @@
+var express = require("express");
+
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+router.get("/", function (req, res) {
+  res.redirect("/burgers");
+});
+
+router.get("/burgers", function (req, res) {
+  //hint: burger.all
+  burger.all((burgerData) => {
+    res.render("index", {burger_data: burgerData});
+  });
+});
+
+// post route -> back to index
+//hint: burger.create
+router.post("/burgers/create", function (req, res) {
+  burger.create(
+    req.body.burger_name, function (result) {
+    // Send back the ID of the new quote
+    console.log(result);
+    res.redirect("/");
+  });
+});
+
+// put route -> back to index
+//hint: burger.update()
+router.put("/burgers/:id", function(req, res) {
+  burger.update(req.params.id, function(result) {
+      // wrapper for orm.js that using MySQL update callback will return a log to console,
+      // render back to index with handle
+      console.log(result);
+      // Send back response and let page reload from .then in Ajax
+      res.sendStatus(200);
+  });
+});
+
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Cheesburger');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Destroyer', true);
